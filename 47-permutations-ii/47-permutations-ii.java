f9dae0e4b39d7ec04276d6fdad84aacafb16c278
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2022-09-11 15:58:58 -0700

    Time: 6 ms (53.66%), Space: 42.8 MB (96.42%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,32 +1,30 @@
 class Solution {
     List<List<Integer>> res = new LinkedList<>();
-    HashSet<List<Integer>> set = new HashSet<>();
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<Integer> dt = new LinkedList<>();
+        Map<Integer, Integer> dt = new HashMap<>();
         List<Integer> permute = new LinkedList<>();
         
         for (int num : nums)
-            dt.add(num);
+            dt.put(num, dt.getOrDefault(num, 0) + 1);
         
         dfs(permute, dt);
         
         return res;
     }
     
-    private void dfs(List<Integer> permute, List<Integer> dt) {
-        if (dt.size() == 0) {
-            if (!set.contains(permute)) {
-                res.add(permute);
-                set.add(permute);
-            }
+    private void dfs(List<Integer> permute, Map<Integer, Integer> dt) {
+        if (dt.isEmpty()) {
+            res.add(permute);
             return;
         }
         
-        for (int i = 0; i < dt.size(); i++) {
-            List<Integer> tempDt = new LinkedList<>(dt);
+        for (int num : dt.keySet()) {
+            Map<Integer, Integer> tempDt = new HashMap<>(dt);
             List<Integer> tempPermute = new LinkedList<>(permute);
-            tempPermute.add(tempDt.get(i));
-            tempDt.remove(tempDt.get(i));
+            tempPermute.add(num);
+            tempDt.put(num, tempDt.get(num) - 1);
+            if (tempDt.get(num) == 0)
+                tempDt.remove(num);
             dfs(tempPermute, tempDt);
         }
     }
