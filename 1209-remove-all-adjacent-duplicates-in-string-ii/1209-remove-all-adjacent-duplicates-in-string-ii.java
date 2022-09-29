650b4c9fe63bc2fe82737298d7f8e22a42397ce9
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2022-09-29 01:07:29 -0700

    Time: 62 ms (64.27%), Space: 42.7 MB (98.95%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        StringBuilder sb = new StringBuilder(s);
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < sb.length(); i++) {
+            if (i == 0 || sb.charAt(i) != sb.charAt(i - 1))
+                stack.push(1);
+            else {
+                int temp = stack.pop() + 1;
+            
+                if (temp == k) {
+                    sb.delete(i - k + 1, i + 1);
+                    i = i - k;
+                }
+                else
+                    stack.push(temp);
+            }
+        }
+        return sb.toString();
+    }
+}
