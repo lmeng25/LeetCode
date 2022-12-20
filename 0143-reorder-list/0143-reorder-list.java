0dd600cb2a545617684a3f1447a86ddefc5531b4
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2022-12-19 17:09:31 -0800

    Time: 15 ms (12.12%), Space: 54.1 MB (48.02%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        // Queue<ListNode> queue = new LinkedList<>();
+
+        ListNode ref1 = head; 
+        ListNode ref2 = head;
+        
+        while (ref2.next != null) {
+            ref1 = ref1.next;
+            ref2 = ref2.next;
+            if (ref2.next != null)
+                ref2 = ref2.next;
+        }
+        
+        while (ref1 != null) {
+            stack.add(ref1);
+            ref1 = ref1.next;
+        }
+        
+        ListNode ref3 = head;
+        while (!stack.isEmpty()) {
+            ListNode temp = stack.pop();
+            temp.next = ref3.next;
+            ref3.next = temp;
+            ref3 = temp.next;
+        }
+        ref3.next = null;
+        
+        // ListNode ref3 = head; 
+        // ListNode ref4 = ref1;
+        
+        // while (ref3 != ref1.next) {
+        //     queue.add(ref3);
+        //     stack.add(ref4);
+        //     ref3 = ref3.next;
+        //     ref4 = ref4.next;
+        // }
+        
+        // head = queue.poll();
+        // ListNode ref5 = head;
+        // while (!stack.isEmpty()) {
+        //     ref5.next = stack.pop();
+        //     ref5 = ref5.next;
+        //     ref5.next = queue.poll();
+        //     ref5 = ref5.next;
+        // }
+        
+        // ref5.next = null;
+    }
+}
