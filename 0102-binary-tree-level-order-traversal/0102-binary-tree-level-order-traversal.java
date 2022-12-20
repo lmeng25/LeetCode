0c3221122674fa7192facac9f733a218a771731b
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2022-12-20 13:46:41 -0800

    Time: 0 ms (100.00%), Space: 42.4 MB (89.19%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        
+        helper(root, res, 0);
+        
+        return res;
+    }
+    
+    private void helper(TreeNode node, List<List<Integer>> res, int level) {
+        if (node != null) {
+            if (level == res.size())
+                res.add(new ArrayList<>());
+            
+            res.get(level).add(node.val);
+            
+            helper(node.left, res, level + 1);
+            helper(node.right, res, level + 1);
+        }
+    }
+}
