62444f3d459c113eeaf0ffe079e6e74fd5b393e6
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2022-12-23 00:04:54 -0800

    Time: 680 ms (5.39%), Space: 117.9 MB (5.78%) - LeetHub

---
+++ 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] numsSameConsecDiff(int n, int k) {
+        Set<Integer> list = new HashSet<>();
+        String nums = "";
+        
+        for (int i = 1; i <= 9; i++) {
+            dfs(i, n - 1, k, list, nums + String.valueOf(i));
+        }
+        
+        int[] res = new int[list.size()];
+        int i = 0;
+        for (int num : list)
+            res[i++] = num;
+        
+        return res;
+    }
+    
+    private void dfs(int i, int n, int k, Set<Integer> list, String nums) {
+        if (n == 0) {
+            list.add(Integer.valueOf(nums));
+            return;
+        }
+        
+        for (int j = 0; j <= 9; j++) {
+            if (j - i == k) {
+                dfs(j, n - 1, k, list, nums + String.valueOf(j));
+                dfs(j, n - 1, -k, list, nums + String.valueOf(j));
+            }
+            if (j - i == -k) {
+                dfs(j, n - 1, k, list, nums + String.valueOf(j));
+                dfs(j, n - 1, -k, list, nums + String.valueOf(j));
+            }
+        }
+    }
+}
