72196c9e2efc052b1e8cdb02024d0cbea967f4bd
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2022-12-22 21:23:04 -0800

    Time: 5 ms (73.81%), Space: 58.4 MB (7.18%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        
+        if (grid == null || grid.length == 0)
+            return res;
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        
+        return res;
+    }
+    
+    private void dfs(char[][] grid, int r, int c) {
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] == '0')
+            return;
+        
+        grid[r][c] = '0';
+        dfs(grid, r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
+    }
+}
