96e1dfd80288431287553b0446b45391cac29a6e
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2023-01-09 11:44:20 -0800

    Time: 1021 ms (12.20%), Space: 180.4 MB (5.04%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        memo = {}
+        def dp(i, j):
+            if i == j:
+                memo[(i, j)] = True
+                return True
+            if i + 1 == j and s[i] == s[j]:
+                memo[(i, j)] = True
+                return True
+            if (i, j) in memo:
+                return memo[(i, j)]
+            else:
+                if s[i] != s[j]:
+                    memo[(i, j)] = False
+                    return False
+                else:
+                    memo[(i, j)] = dp(i + 1, j - 1)
+                return memo[(i, j)]
+        
+        res = 0
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                if dp(i, j):
+                    res += 1
+                    
+        return res
