96113d921254f6c4fa2de9faefcdc9ea5bb56867
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2023-01-11 10:54:25 -0800

    Time: 34 ms (84.11%), Space: 14.1 MB (29.02%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        memo = {}
+        
+        def dp(i):
+            if i == len(s):
+                return 1
+            if s[i] == '0':
+                return 0
+            if i == len(s) - 1:
+                return 1
+
+            if i not in memo:
+                if int(s[i:i+2]) <= 26:
+                    memo[i] = dp(i + 1) + dp(i + 2)
+                else:
+                    memo[i] = dp(i + 1)
+            return memo[i]
+            
+        return dp(0)
+                
