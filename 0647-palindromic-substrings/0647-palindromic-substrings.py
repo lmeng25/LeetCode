365d4155c6ba699d0099a119ad76515eff41905d
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2023-01-11 07:28:27 -0800

    Time: 277 ms (52.23%), Space: 22 MB (19.98%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,24 +1,12 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        memo = {}
-        def dp(i, j):
-            if j - i + 1 <= 3:
-                return s[i] == s[j]
-            
-            if (i, j) in memo:
-                return memo[(i, j)]
-            else:
-                if s[i] != s[j]:
-                    memo[(i, j)] = False
-                    return False
-                else:
-                    memo[(i, j)] = dp(i + 1, j - 1)
-                return memo[(i, j)]
-        
+        n = len(s)
+        memo = [[0] * n for _ in range(n)]
         res = 0
-        for i in range(len(s)):
-            for j in range(len(s) - 1, i - 1, -1):
-                if dp(i, j):
-                    res += 1
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                memo[i][j] = s[i] == s[j] and (j - i + 1 <= 3 or memo[i + 1][j - 1])
+                res += memo[i][j]
                     
         return res
