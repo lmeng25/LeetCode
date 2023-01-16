e768897b7009b23c98c0872306e956480b52c617
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2023-01-16 07:09:44 -0800

    Time: 51 ms (53.60%), Space: 14.1 MB (44.17%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        memo = {}
+        def dp(i):
+            if i > len(s) - 1:
+                return True
+            if i not in memo:
+                for j in range(i + 1, len(s) + 1):
+                    temp = s[i:j]
+                    if s[i:j] in wordDict:
+                        if dp(j):
+                            memo[i] = True
+                            return True
+                memo[i] = False
+            return memo[i]
+        
+        return dp(0)
+                    
+            
+            
