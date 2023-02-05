258d027f0314bd1b92e0ff3ebb887dc97ef9569c
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2023-02-05 08:59:36 -0800

    Time: 102 ms (87.44%), Space: 18.6 MB (68.01%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,16 +1,4 @@
-from queue import PriorityQueue
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        map = {}
-        for num in nums:
-            if num in map:
-                map[num] = map[num] + 1
-            else:
-                map[num] = 1
-        
-        sorted_list = sorted(map.items(), key = lambda item: item[1], reverse = True)
-        first_k = sorted_list[:k]
-        res = [item[0] for item in first_k]
-        
-        return res
-        
+        count = Counter(nums)
+        return [item[0] for item in count.most_common(k)]
