d4e0f6c49487eab0733764985b172443d95b4f53
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2023-02-09 12:31:33 -0800

    Time: 1205 ms (68.22%), Space: 18.3 MB (50.55%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -7,13 +7,16 @@ class Solution:
             l = i + 1
             r = len(nums) - 1
             while l < r:
-                if nums[l] + nums[r] + nums[i] == 0:
-                    temp = [nums[i], nums[l], nums[r]]
-                    if temp not in res:
-                        res.append(temp)
+                sum = nums[l] + nums[r] + nums[i]
+                if sum == 0:
+                    res.append([nums[i], nums[l], nums[r]])
                     l += 1
                     r -= 1
-                elif nums[l] + nums[r] + nums[i] > 0:
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
+                elif sum > 0:
                     r -= 1
                 else:
                     l += 1
@@ -21,6 +24,8 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] > 0:
                 break
-            twoSum(i)
+            
+            if i == 0 or nums[i] != nums[i - 1]:
+                twoSum(i)
         
         return res
