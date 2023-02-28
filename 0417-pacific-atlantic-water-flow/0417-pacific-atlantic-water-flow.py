2ffd1e84f5dd81babca00f9ddcccbf0bc9705a28
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2023-02-28 14:33:22 -0800

    Time: 270 ms (91.15%), Space: 15.5 MB (76.35%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,20 +1,22 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        n = len(heights)
-        m = len(heights[0])
         pac = set()
         atl = set()
+        n = len(heights)
+        m = len(heights[0])
         
         def dfs(i, j, visited, lastHeight):
-            if i < 0 or j < 0 or i == n or j == m or heights[i][j] < lastHeight:
-                return
             if (i, j) in visited:
                 return
+            if i < 0 or j < 0 or i > n - 1 or j > m - 1:
+                return
+            if heights[i][j] < lastHeight:
+                return
             visited.add((i, j))
-            dfs(i - 1, j, visited, heights[i][j])
             dfs(i + 1, j, visited, heights[i][j])
-            dfs(i, j - 1, visited, heights[i][j])
+            dfs(i - 1, j, visited, heights[i][j])
             dfs(i, j + 1, visited, heights[i][j])
+            dfs(i, j - 1, visited, heights[i][j])
         
         for i in range(n):
             dfs(i, 0, pac, 0)
@@ -23,12 +25,10 @@ class Solution:
         for j in range(m):
             dfs(0, j, pac, 0)
             dfs(n - 1, j, atl, 0)
-        
+            
         res = []
         for item in pac:
             if item in atl:
                 res.append(item)
-        
+                
         return res
-        
-        
