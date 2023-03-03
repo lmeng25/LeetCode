9e54985ffb4866c6eae943a8b4b2bbf609c2e8e8
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2023-03-03 08:46:43 -0800

    Time: 682 ms (64.14%), Space: 13.8 MB (86.17%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,16 +1,36 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        memo = [[0] * n for _ in range(n)]
-                
-        maxLen = 0
-        res = ""
-        for i in range(n - 1, -1, -1):
-            for j in range(i, len(s)):
-                memo[i][j] = s[i] == s[j] and (j - i + 1 <= 3 or memo[i + 1][j - 1])
-                if memo[i][j]:
-                    if j - i + 1 > maxLen:
-                        res = s[i : j + 1]
-                        maxLen = j - i + 1
+        res = s[0]
+        lenRes = 1
+        
+        i = 0
+        while i < len(s):
+            # odd length
+            k = i
+            j = i + 1
+            while k >= 0 and j < len(s):
+                if s[k] == s[j]:
+                    if (j - k + 1) > lenRes:
+                        res = s[k : j + 1]
+                        lenRes = len(res)
+                    k -= 1
+                    j += 1
+                else:
+                    break
+
+            # even length
+            k = i
+            j = i
+            while k >= 0 and j < len(s):
+                if s[k] == s[j]:
+                    if (j - k + 1) > lenRes:
+                        res = s[k : j + 1]
+                        lenRes = len(res)
+                    k -= 1
+                    j += 1
+                else:
+                    break
+
+            i += 1
+
         return res
-            
