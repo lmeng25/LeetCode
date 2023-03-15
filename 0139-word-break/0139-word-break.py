848b430d3d89e60699c2dd3cf8d1bbb2e09605d5
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2023-03-15 12:54:30 -0700

    Time: 80 ms (5.53%), Space: 13.8 MB (91.20%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,19 +1,12 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        memo = {}
-        def dp(i):
-            if i > len(s) - 1:
-                return True
-            if i not in memo:
-                for j in range(i + 1, len(s) + 1):
-                    if s[i:j] in wordDict:
-                        if dp(j):
-                            memo[i] = True
-                            return True
-                memo[i] = False
-            return memo[i]
+        dp = [0 for i in range(len(s) + 1)]
+        dp[-1] = 1
         
-        return dp(0)
-                    
+        for i in range(len(s) - 1, -1, -1):
+            for word in wordDict:
+                if (i + len(word)) <= len(s) and s[i : i + len(word)] in wordDict and dp[i + len(word)] == 1:
+                    dp[i] = 1
+                    break;
             
-            
+        return dp[0]
