7f706307ac974a9e97b4dada97759976c3433380
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2023-03-21 10:27:26 -0700

    Time: 49 ms (20.53%), Space: 14.1 MB (17.32%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        def dfs(perm, arr):
+            if len(perm) == len(nums):
+                res.append(list(perm))
+                return
+            for j in arr:
+                tPerm = list(perm)
+                tArr = list(arr)
+                tPerm.append(j)
+                tArr.remove(j)
+                dfs(tPerm, tArr)
+                tPerm.pop()
+                tArr.append(j)
+        
+        dfs([], nums)
+        return res
