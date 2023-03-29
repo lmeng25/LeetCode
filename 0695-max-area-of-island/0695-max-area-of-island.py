988c291abdbdfb280c4189dd572348f41d7080da
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2023-03-29 10:18:47 -0700

    Time: 136 ms (70.48%), Space: 16.6 MB (46.58%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -7,7 +7,6 @@ class Solution:
         def dfs(i, j, area):
             nonlocal res
             if i < 0 or j < 0 or i >= n or j >= m or grid[i][j] != 1:
-                res = max(res, area)
                 return area
             grid[i][j] = 0
             area += 1
@@ -15,6 +14,7 @@ class Solution:
             area = dfs(i - 1, j, area)
             area = dfs(i, j + 1, area)
             area = dfs(i, j - 1, area)
+            res = max(res, area)
             return area
         
         for i in range(n):
