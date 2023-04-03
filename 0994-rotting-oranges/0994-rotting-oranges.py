e2c1f66b01d0aae2061e15f96787205752d9d5f1
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2023-04-03 07:23:45 -0700

    Time: 47 ms (90.30%), Space: 13.9 MB (85.28%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -2,41 +2,28 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         n = len(grid)
         m = len(grid[0])
-        
-        def rot(i, j, num):
-            if i < 0 or j < 0 or i >= n or j >= m:
-                return
-            if grid[i][j] == 1:
-                grid[i][j] = num + 1
-            return
-        
-        haveOne = False
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == 1:
-                    haveOne = True
-                    break
-        if not haveOne:
-            return 0
-        
-        start = 2
-        stop = False
-        while not stop:
-            stop = True
-            for i in range(n):
-                for j in range(m):
-                    if grid[i][j] == start:
-                        stop = False
-                        rot(i - 1, j, start)
-                        rot(i + 1, j, start)
-                        rot(i, j - 1, start)
-                        rot(i, j + 1, start)
-            start += 1
+        fresh = 0
+        rotten = deque()
+        time = 0
         
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 1:
-                    return -1
+                    fresh += 1
+                if grid[i][j] == 2:
+                    rotten.append([i, j])
         
-        return start - 2 - 2
-                        
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        while rotten and fresh:
+            for k in range(len(rotten)):
+                i, j = rotten.popleft()
+                for dr, dc in directions:
+                    ni = i + dr
+                    nj = j + dc
+                    if ni >= 0 and nj >= 0 and ni < n and nj < m and grid[ni][nj] == 1:
+                        grid[ni][nj] = 2
+                        fresh -= 1
+                        rotten.append([ni, nj])
+            time += 1
+        
+        return time if fresh == 0 else -1
