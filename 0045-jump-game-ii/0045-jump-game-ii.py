5cfc1672dbe562c3c1fceb7419db21cd30347a20
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2023-04-10 13:41:47 -0700

    Time: 125 ms (86.63%), Space: 15.1 MB (49.29%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,17 +1,18 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        res = 0
-        l = 0
-        r = 0
+        if len(nums) == 1:
+            return 0
         
+        l = 0
+        r = nums[0]
+        
+        res = 1
         while r < len(nums) - 1:
-            farmost = r
-            for i in range(l, r + 1):
-                farmost = max(farmost, i + nums[i])
-            l = r + 1
-            r = farmost
+            temp = r
             res += 1
-            
+            for i in range(l, r + 1):
+                r = max(r, i + nums[i])
+                if r >= len(nums) - 1:
+                    return res
+            l = temp + 1
         return res
-            
-            
