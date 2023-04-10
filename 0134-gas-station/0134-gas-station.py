b00a54402dfb0d3dd0d2e118721b3838825e224a
Lei Meng <98987608+lmeng25@users.noreply.github.com>
2023-04-10 15:22:45 -0700

    Time: 1093 ms (45.65%), Space: 19.7 MB (79.45%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -3,14 +3,13 @@ class Solution:
         if sum(gas) < sum(cost):
             return -1
         
-        diff = [gas[i] - cost[i] for i in range(len(gas))]
         total = 0
         res = 0
-        for i in range(len(diff)):
-            if diff[i] + total < 0:
+        for i in range(len(gas)):
+            if gas[i] - cost[i] + total < 0:
                 total = 0
                 res = i + 1
             else:
-                total += diff[i]
+                total += gas[i] - cost[i]
         
         return res
